5d60e84f62c2e73fcf1bea1f4e9c4860084ea474
Surya <suryask7549@gmail.com>
2024-10-21 22:42:42 +0530

    DSA avg of odd elements in Array

--- DSA_Part_1/average_odd_elements.java
+++ DSA_Part_1/average_odd_elements.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class average_odd_elements {
-    int sum;
 
     public static void main(String[] args) {
-
-        int a = 10;
-        average_odd_elements obj = new average_odd_elements();
-        System.out.println(obj.sum);
+        int size, odd_count;
+        odd_count = 0;
+        int sum = 0;
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the size of array ");
+        size = obj.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter elements in array ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = obj.nextInt();
+            if (!(arr[i] % 2 == 0)) {
+                sum += arr[i];
+                odd_count++;
+            }
+        }
+        System.out.println("The sum is : " + sum + "\nThe average is : " + ((double) sum / odd_count));
+        obj.close();
     }
 }
