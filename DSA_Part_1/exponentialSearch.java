36499ceeca5b4da22cdad6a218faea9ea40c86b9
Surya <suryask7549@gmail.com>
2024-11-25 22:00:16 +0530

    Add exponentialSearch.java

---
+++ DSA_Part_1/exponentialSearch.java
@@ -0,0 +1,9 @@
+public class exponentialSearch {
+  public static void main(String[] args) {
+    
+  }
+  static int expoSearch(int arr[], int min,int max,int target){
+    
+    return -1;
+  }  
+}
